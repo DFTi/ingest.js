2ff16df5370a0ab21b2bea12e1f7645ef66eb588
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-28 19:05:02 -0700

    further progress on server

--- server.js
+++ server.js
@@ -2,18 +2,57 @@ var PORT = process.env.port || 1337;
 var md5 = require("./common/md5").md5;
 var http = require("http");
 var url  = require("url");
+var fs = require('fs');
+var path = require('path');
+var tempDirectory = path.resolve('./tmp');
 
-var routes = {
-  fileEvents: /^\/transfers\/(.+)\/events$/
+var requestors = {};
+
+var Requestor = function(id, res) {
+  this.id = id;
+  this.bin = path.join(tempDirectory, this.id+".bin");
+  this.json = path.join(tempDirectory, this.id+".json");
+  this.res = res;
+//    res.write("event: message\n");
+//    res.write("data: "+JSON.stringify({foo: "bar"})+"\n\n");
+  this.init();
 };
 
+Requestor.prototype = {
+  init: function() {
+    fs.stat(this.bin, function(err, stats) {
+      if (err) {
+        console.log("it does not exist");
+        //   no? request some chunks
+      } else {
+        console.log("it exists", stats);
+        //    foreach chunk emit progress
+        //    any missing chunks?
+        //      no? file is done
+        //      yes? 
+      }
+    })
+  },
+
+  /* Accept a chunk from the multipart request object */
+  receiveChunk: function(chunkNumber, req) {
+    console.log("receivechunk", chunkNumber, req);
+  }
+};
+
+var routes = {
+  events: /^\/transfers\/(.+)\/events$/,
+  chunks: /^\/transfers\/(.+)\/chunks\/(\d+)$/
+};
 http.createServer(function (req, res) {
-  var pathname = url.parse(req.url).pathname;
-  var match = pathname.match(routes.fileEvents);
-  if (match === null) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("You know, for uploads");
-  } else {
+  var parsedURL = url.parse(req.url);
+  var pathname = parsedURL.pathname;
+  var match = null;
+  if (match = pathname.match(routes.chunks)) {
+    var id = match[1];
+    var rx = requestors[match[1]];
+    rx.receiveChunk(match[2], req);
+  } else if (match = pathname.match(routes.events)) {
     res.writeHead(200, {
       "Content-Type":"text/event-stream",
       "Cache-Control":"no-cache",
@@ -23,12 +62,26 @@ http.createServer(function (req, res) {
       "Access-Control-Allow-Headers":"Content-Type",
     });
     res.write("retry: 1000\n");
-    res.write("event: message\n");
-    res.write("data: "+JSON.stringify({foo: "bar"})+"\n\n");
+    var id = match[1];
+    requestors[id] = null;
+    requestors[id] = new Requestor(id, res);
     req.connection.addListener("close", function() {
-      console.log("Connnection closed");
+      requestors[id] = null;
     });
+  } else {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("You know, for uploads");
   }
 }).listen(PORT);
 console.log("Server running at http://0.0.0.0:"+PORT+"/");
 
+// Setup temp directory
+fs.stat(tempDirectory, function(err, stats) {
+  if (err) {
+    fs.mkdir(tempDirectory);
+  } else {
+    if (!stats.isDirectory()) {
+      throw new Error(tempDirectory+" is not a directory!");
+    }
+  }
+});
