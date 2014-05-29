a5a094d52692cf89bf7edf5bec0b796237912a38
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-28 22:13:33 -0700

    update serverside

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var url  = require("url");
 var fs = require('fs');
 var path = require('path');
 var tempDirectory = path.resolve('./tmp');
+var Dicer = require('dicer');
+var RE_BOUNDARY = /^multipart\/.+?(?:; boundary=(?:(?:"(.+)")|(?:([^\s]+))))$/i;
+var inspect = require('util').inspect;
+
+var CHUNKSIZE = 32768;
 
 var requestors = {};
 
@@ -39,8 +44,25 @@ Requestor.prototype = {
   },
 
   /* Accept a chunk from the multipart request object */
-  receiveChunk: function(chunkNumber, req) {
-    console.log("receive chunk ", chunkNumber, req);
+  receiveChunk: function(chunkNumber, chunkMd5, req, done) {
+    var m = RE_BOUNDARY.exec(req.headers['content-type']);
+    var d = new Dicer({ boundary: m[1] || m[2] });
+    //var writeStream = fs.createWriteStream(this.bin);
+
+    d.on('part', function(p) {
+      //console.log('New part!');
+      p.on('data', function(data) {
+        console.log(data[0]);
+      });
+      p.on('end', function() {
+        //console.log('End of part\n');
+      });
+    });
+    d.on('finish', function() {
+      console.log(chunkMd5);
+      done();
+    });
+    req.pipe(d);
   },
 
   _emit: function(event, data) {
@@ -52,18 +74,31 @@ Requestor.prototype = {
 
 var routes = {
   events: /^\/transfers\/(.+)\/events$/,
-  chunks: /^\/transfers\/(.+)\/chunks\/(\d+)$/
+  chunks: /^\/transfers\/(.+)\/chunks\/(\d+)\/(.+)$/
 };
 http.createServer(function (req, res) {
   var parsedURL = url.parse(req.url);
   var pathname = parsedURL.pathname;
   var match = null;
-  if (match = pathname.match(routes.chunks)) {
-    console.log("receiving chunk?");
+  if ((match = pathname.match(routes.chunks)) && req.method === "POST") {
     var id = match[1];
+    var chunkNumber = match[2];
+    var chunkMd5 = match[3];
     var rx = requestors[match[1]];
-    rx.receiveChunk(match[2], req);
-  } else if (match = pathname.match(routes.events)) {
+    if (rx === null) { res.writeHead(500); res.end(); }
+    else {
+      rx.receiveChunk(chunkNumber, chunkMd5, req, function() {
+        res.writeHead(200, {
+          "Content-Type":"text/plain",
+          "Cache-Control":"no-cache",
+          "Access-Control-Allow-Origin":"*",
+          "Access-Control-Allow-Methods":"GET,PUT,POST,DELETE",
+          "Access-Control-Allow-Headers":"Content-Type",
+        });
+        res.end('OK');
+      });
+    }
+  } else if ((match = pathname.match(routes.events)) && req.method === "GET") {
     res.writeHead(200, {
       "Content-Type":"text/event-stream",
       "Cache-Control":"no-cache",
